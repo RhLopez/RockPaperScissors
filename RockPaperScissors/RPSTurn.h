3e0874a51de0507541764d50e896baf6a3d9e392
Ramiro H. Lopez <RhL@ramiros-imac.fios-router.home>
2017-01-12 22:47:59 -0800

    add: RPSTurn class

---
+++ RockPaperScissors/RPSTurn.h
@@ -0,0 +1,23 @@
+//
+//  RPSTurn.h
+//  RockPaperScissors
+//
+//  Created by Ramiro H. Lopez on 1/12/17.
+//  Copyright © 2017 Ramiro H. Lopez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, Move) {
+    Rock,
+    Paper,
+    Scissors
+};
+
+@interface RPSTurn : NSObject
+
+@property (nonatomic) Move move;
+
+-(instancetype)initWithMove:(Move)move;
+
+@end
---
+++ RockPaperScissors/RPSTurn.m
@@ -0,0 +1,23 @@
+//
+//  RPSTurn.m
+//  RockPaperScissors
+//
+//  Created by Ramiro H. Lopez on 1/12/17.
+//  Copyright © 2017 Ramiro H. Lopez. All rights reserved.
+//
+
+#import "RPSTurn.h"
+
+@implementation RPSTurn
+
+-(instancetype)initWithMove:(Move)move {
+    self = [super init];
+    
+    if (self) {
+        _move = move;
+    }
+    
+    return self;
+}
+
+@end
