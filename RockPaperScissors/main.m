7afce1288877d1b8db195220310e066ac05c288a
Ramiro H. Lopez <RhL@ramiros-imac.fios-router.home>
2017-01-12 22:37:15 -0800

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Ramiro H. Lopez on 1/12/17.
+//  Copyright © 2017 Ramiro H. Lopez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
