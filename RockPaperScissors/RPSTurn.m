d9f192d0cb094823c5c87e12d195380bc5be5b42
Ramiro H. Lopez <RhL@ramiros-imac.fios-router.home>
2017-01-13 23:10:26 -0800

    add: description()

--- RockPaperScissors/RPSTurn.m
+++ RockPaperScissors/RPSTurn.m
@@ -47,8 +47,6 @@
             return Invalid;
             break;
     }
-    
-    return Rock;
 }
 
 -(BOOL)defeats:(RPSTurn*)opponent {
@@ -60,9 +58,23 @@
     } else {
         return false;
     }
-    
-    
-    return false;
+}
+
+-(NSString*)description {
+    switch (self.move) {
+        case 0:
+            return @"Rock";
+            break;
+        case 1:
+            return @"Paper";
+            break;
+        case 2:
+            return @"Scissors";
+            break;
+        default:
+            return @"Invalid";
+            break;
+    }
 }
 
 @end
